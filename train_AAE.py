c67074c31f126a0ec9c4b676a91b5274999fd65d
ranya almohsen <ranyaalmohsen@ranyas-MacBook-Pro.local>
2018-12-18 14:45:31 -0500

    fix

--- train_AAE.py
+++ train_AAE.py
@@ -1,4 +1,4 @@
-# Copyright 2018 Stanislav Pidhorskyi
+# Copyright 2018 Ranya Almohsen
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
