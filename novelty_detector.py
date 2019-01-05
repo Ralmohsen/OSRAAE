688b870e623b65ce4a05b41ce6c6f7e26dc09660
ranya <ralmohse@mix.wvu.edu>
2019-01-05 18:30:53 -0500

    update copy right

--- novelty_detector.py
+++ novelty_detector.py
@@ -1,3 +1,4 @@
+# Copyright 2018 Ranya Almohsen
 # Copyright 2018 Stanislav Pidhorskyi
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,7 +14,6 @@
 # limitations under the License.
 # ==============================================================================
 
-# Copyright 2018 Ranya Almohsen
 
 from __future__ import print_function
 import torch.utils.data
